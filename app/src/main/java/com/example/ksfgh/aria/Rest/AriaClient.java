b4c694cfc7fdd43226f5272ffe3a833a928f6da4
k-francisco <piattosnovalays@gmail.com>
2017-12-08 03:41:12 +0800

    can upload photos to the server

--- app/src/main/java/com/example/ksfgh/aria/Rest/AriaClient.java
+++ app/src/main/java/com/example/ksfgh/aria/Rest/AriaClient.java
@@ -7,11 +7,15 @@ import com.example.ksfgh.aria.Model.FacebookUserModel;
 
 import java.util.List;
 
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
+import retrofit2.http.Part;
 
 /**
  * Created by ksfgh on 16/11/2017.
@@ -27,4 +31,12 @@ public interface AriaClient {
 
     @POST("createBand")
     Call<BandMemberModel> createBand(@Body BandCreationModel bandCreationModel);
+
+    @Multipart
+    @POST("editbandPic")
+    Call<ResponseBody> editBandPic(
+            @Part("bandId") RequestBody bandId,
+            @Part MultipartBody.Part pic
+    );
+
 }
--- app/src/main/java/com/example/ksfgh/aria/View/activities/MainActivity.java
+++ app/src/main/java/com/example/ksfgh/aria/View/activities/MainActivity.java
@@ -1,9 +1,17 @@
 package com.example.ksfgh.aria.View.activities;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Button;
@@ -32,6 +40,8 @@ import com.google.gson.Gson;
 
 import org.json.JSONObject;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
@@ -40,6 +50,9 @@ import java.util.Arrays;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -63,6 +76,7 @@ public class MainActivity extends AppCompatActivity {
     FacebookCallback facebookCallback;
     private AccessTokenTracker accessTokenTracker;
     private ProfileTracker profileTracker;
+    private boolean writeAccepted = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -108,6 +122,9 @@ public class MainActivity extends AppCompatActivity {
                             @Override
                             public void onCompleted(JSONObject object, GraphResponse response) {
                                 try {
+
+                                    Log.d("fb",response.toString());
+                                    Log.d("fb",object.toString());
                                     FacebookUserModel user = new FacebookUserModel(
                                             profile.getId(),
                                             profile.getFirstName(),
@@ -155,18 +172,62 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == Singleton.PICK_PHOTO && resultCode == Activity.RESULT_OK) {
-            if (data == null) {
-                return;
-            }
+        if (requestCode == Singleton.PICK_PHOTO && resultCode == Activity.RESULT_OK && data != null) {
             try {
-                InputStream inputStream = getApplicationContext().getContentResolver().openInputStream(data.getData());
-            } catch (FileNotFoundException e) {
+                Uri selectedImage = data.getData();
+                String[] filePathColumn = { MediaStore.Images.Media.DATA };
+                Cursor cursor = getContentResolver().query(selectedImage,filePathColumn, null, null, null);
+                cursor.moveToFirst();
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                String picturePath = cursor.getString(columnIndex);
+                cursor.close();
+
+                uploadPhoto(selectedImage, picturePath);
+
+            } catch (Exception e) {
                 e.printStackTrace();
                 Log.d("pick photo error", e.getMessage());
             }
         }
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode){
+            case 200:
+                boolean writeAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                Intent intent = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                intent.setType("image/*");
+                startActivityForResult(intent, Singleton.PICK_PHOTO);
+                break;
+        }
+    }
+
+    private void uploadPhoto(Uri selectedImage, String imagePath) {
+
+        RequestBody description = RequestBody.create(MultipartBody.FORM, "6");
+        File originalFile = new File(imagePath);
+        RequestBody filePart = RequestBody.create(MediaType.parse(getContentResolver().getType(selectedImage)), originalFile);
+        MultipartBody.Part file = MultipartBody.Part.createFormData("bandPic", originalFile.getName(), filePart);
+
+        Call<ResponseBody> call = RetrofitClient.getClient().editBandPic(description, file);
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+
+                Log.d("photo", response.message());
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+
+            }
+        });
+
+    }
+
 
     @OnClick(R.id.btnGetBands)
     public void getBands() {
@@ -223,9 +284,17 @@ public class MainActivity extends AppCompatActivity {
 
     @OnClick(R.id.btnPickPhoto)
     public void pickPhoto() {
-        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-        intent.setType("image/*");
-        startActivityForResult(intent, Singleton.PICK_PHOTO);
+
+        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1){
+            String[] perms = {"android.permission.WRITE_EXTERNAL_STORAGE"};
+            int permsRequestCode = 200;
+            requestPermissions(perms, permsRequestCode);
+        }
+        else {
+            Intent intent = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            intent.setType("image/*");
+            startActivityForResult(intent, Singleton.PICK_PHOTO);
+        }
     }
 
     @OnClick(R.id.btnCreateBand)
@@ -234,7 +303,7 @@ public class MainActivity extends AppCompatActivity {
         FacebookUserModel model = new Gson().fromJson(getSharedPreferences(Singleton.PREFERENCE_NAME, MODE_PRIVATE).getString("user", null),
                 FacebookUserModel.class);
 
-        BandCreationModel newBand = new BandCreationModel(model.user_id.toString(), "Drummer", "generation d", 1, 3, "this is a band");
+        BandCreationModel newBand = new BandCreationModel(model.user_id.toString(), "Drummer", "generation g", 1, 3, "this is a band");
 
         Call<BandMemberModel> call = RetrofitClient.getClient().createBand(newBand);
 
@@ -255,4 +324,5 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
 }
